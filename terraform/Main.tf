cc4d46604701081b7aabf324d85d0b3d4b14f94e
JayashanNawarathne <jayashannawarathna64@gmail.com>
2026-03-26 15:08:10 +0530

    final change

--- terraform/Main.tf
+++ terraform/Main.tf
@@ -29,7 +29,7 @@ data "aws_ami" "ubuntu" {
 
 # ------------------ SECURITY GROUP ------------------
 resource "aws_security_group" "app_sg" {
-  name        = "srilanka_project_sg_v12 "
+  name        = "srilanka_project_sg_v13 "
   description = "Allow SSH, Backend, Frontend"
 
   ingress {
